f3a39d0e9610d7d5495a2edf7164500a569758f6
Tushar-Mishra <tusharmishra9695@gmail.com>
2023-03-02 18:08:59 +0530

    #03 Timer logic created successfully

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import "./App.css";
+import React, { useState, useEffect } from "react";
 import TaskList from "./components/TaskList";
 function App() {
+  const [isStart, setStart] = useState(false);
+  const [isPause, setPause] = useState(false);
+  const [calTime, setCalTime] = useState(0);
+  useEffect(() => {
+    let cal_interval = 0;
+    if (isStart && isPause === false) {
+      cal_interval = setInterval(() => {
+        setCalTime((calTime) => calTime + 1);
+      }, 1000);
+    } else {
+      clearInterval(cal_interval);
+    }
+    return () => {
+      clearInterval(cal_interval);
+    };
+  }, [isStart, isPause]);
+
+  const manageStart = () => {
+    setStart(true);
+    setPause(false);
+  };
+  const managePause = () => {
+    setPause(!isPause);
+  };
   return (
     <div className="App">
       <div className="outer">
         <div className="inner_flex">
           <div>
-            <h1 className="texting">06 </h1>
+            <h1 className="texting">{calTime} </h1>
             <span>HOURS</span>
           </div>{" "}
           <div>
@@ -20,16 +45,24 @@ function App() {
         </div>
       </div>
       <div className="inner_flex">
-        <span className="btn_style">Start</span>
-        <span className="btn_style">Pause</span>
-        <span className="btn_style">Save</span>
+        <button className="btn_style" onClick={manageStart}>
+          Start
+        </button>
+        <button className="btn_style" onClick={managePause}>
+          Pause
+        </button>
+        <button className="btn_style">Save</button>
       </div>
       <div className="outer_task ">
         <div className="inner_flex">
           <span>Title</span>
           <span> Time Tracked</span>
         </div>
-        <TaskList />
+        {calTime ? (
+          <TaskList />
+        ) : (
+          <span className="no_data">No Data Found !</span>
+        )}
       </div>
     </div>
   );
