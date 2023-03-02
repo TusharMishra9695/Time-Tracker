4aa9e88752753a5daa3fdf227312ef9972e72cab
Tushar-Mishra <tusharmishra9695@gmail.com>
2023-03-02 17:31:13 +0530

    #02 Styling and UI completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
-import logo from "./logo.svg";
 import "./App.css";
-
+import TaskList from "./components/TaskList";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="outer">
+        <div className="inner_flex">
+          <div>
+            <h1 className="texting">06 </h1>
+            <span>HOURS</span>
+          </div>{" "}
+          <div>
+            <h1 className="texting"> 00 </h1>
+            <span>MINUTES</span>
+          </div>{" "}
+          <div>
+            <h1 className="texting">14</h1>
+            <span>SECONDS</span>
+          </div>
+        </div>
+      </div>
+      <div className="inner_flex">
+        <span className="btn_style">Start</span>
+        <span className="btn_style">Pause</span>
+        <span className="btn_style">Save</span>
+      </div>
+      <div className="outer_task ">
+        <div className="inner_flex">
+          <span>Title</span>
+          <span> Time Tracked</span>
+        </div>
+        <TaskList />
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/TaskList.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function TaskList() {
+  return (
+    <div className="inner_flex list">
+      <span>Title</span>
+      <span> 01 : 23 : 12</span>
+    </div>
+  );
+}
